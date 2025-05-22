6f96d747e1b9fee5e27a03e7fec868cad6a25c5a
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-22 20:37:45 +0200

    feat(singleton): adding comments

--- design-patterns/Singleton/Singleton.cs
+++ design-patterns/Singleton/Singleton.cs
@@ -2,8 +2,12 @@ namespace design_patterns.Singleton;
 
 public sealed class Singleton
 {
+    // Using Lazy to not instantiate the object if not used
     private static Lazy<Singleton> _instance = new(() => new Singleton());
+    
+    // Global access point to the unique Singleton instance 
     public static Singleton Instance => _instance.Value;
 
+    // Constructor private so the Singleton to prevent external instantiation
     private Singleton() { }
 }
