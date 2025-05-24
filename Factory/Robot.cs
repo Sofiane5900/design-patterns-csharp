5e986879bb4c750187d72a30b3d554c65df03d28
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-24 14:13:15 +0200

    feat(factory): adding GetRobotNumber method

--- Factory/Robot.cs
+++ Factory/Robot.cs
@@ -2,5 +2,13 @@ namespace design_patterns.Factory;
 
 public class Robot
 {
-    public string? RobotNumber { get; set; }
+    public string RobotNumber { get; set; }
+
+    public Factory GetRobotNumber(Robot r)
+    {
+        if (RobotNumber.StartsWith("1")) return new FactoryImplementationOne();
+        if (RobotNumber.StartsWith("2")) return new FactoryImplementationTwo();
+        if (RobotNumber.StartsWith("3")) return new FactoryImplementationThree();
+        throw new InvalidOperationException("Invalid robot number prefix.");
+    }
 }
