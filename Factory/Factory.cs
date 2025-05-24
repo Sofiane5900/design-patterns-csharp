41882fdfe6a33955545786865b1d655c676d118d
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-24 14:01:30 +0200

    feat(factory): adding 2 more concrete classes that implements the abstract one factory

--- Factory/Factory.cs
+++ Factory/Factory.cs
@@ -5,10 +5,26 @@ public abstract class Factory
     public abstract void CreateProduct();
 }
 
-public class FactoryImplementation : Factory
+public class FactoryImplementationOne : Factory
 {
     public override void CreateProduct()
     {
-       Console.WriteLine("Factory Pattern");
+       Console.WriteLine("1");
+    }
+}
+
+public class FactoryImplementationTwo : Factory
+{
+    public override void CreateProduct()
+    {
+        Console.WriteLine("2");
+    }
+}
+
+public class FactoryImplementationThree : Factory
+{
+    public override void CreateProduct()
+    {
+        Console.WriteLine("3");
     }
 }
