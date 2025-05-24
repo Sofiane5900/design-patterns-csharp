4ed3dbd210019daec42b0e75da3b2c4d2153c4f8
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-24 14:16:31 +0200

    feat(factory): instancing 3 robot

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using design_patterns.Factory;
 using design_patterns.Singleton;
 
 // Singleton
 Singleton instance1 = Singleton.Instance;
 Singleton instance2 = Singleton.Instance;
 Console.WriteLine(ReferenceEquals(instance1,instance2));
+
+
+// Factory
+Robot robotOne = new Robot("1");
+Robot robotTwo = new Robot("2");
+Robot robotThree = new Robot("3");
