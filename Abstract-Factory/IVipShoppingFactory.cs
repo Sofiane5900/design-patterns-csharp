5ef0754b59d44f3feadaa1869b532442ac7771f4
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-29 14:28:06 +0200

    feat(abstract): adding prioritycommand vip and standard

--- Abstract-Factory/IVipShoppingFactory.cs
+++ Abstract-Factory/IVipShoppingFactory.cs
@@ -4,11 +4,23 @@ public interface IPriorityCommand
 {
     int Priority { get;}
 }
-
 public interface IPurcentReduction
 {
-    
+    decimal Purcentage { get; }
 }
+
+public class StandardPriorityCommand : IPriorityCommand
+{
+    public int Priority => 0;
+}
+
+public class VipPriorityCommand : IPriorityCommand
+{
+    public int Priority => 1;
+}
+
+
 public interface IVipShoppingFactory
 {
+    
 }
