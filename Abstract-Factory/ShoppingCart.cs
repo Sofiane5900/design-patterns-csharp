ce94092242f8bee1fb37c9d7bb2fd2b3143485d2
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-29 13:55:24 +0200

    feat(abstract): adding customer class with orders property

--- Abstract-Factory/ShoppingCart.cs
+++ Abstract-Factory/ShoppingCart.cs
@@ -1,5 +1,10 @@
 namespace design_patterns.Abstract_Factory;
 
+
+public class Customer
+{
+    public int[] Orders { get; set; } = Array.Empty<int>();
+}
 public class ShoppingCart
 {
     public void CreateCommand()
