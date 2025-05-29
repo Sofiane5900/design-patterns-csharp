dcc8abcdf65032330919c370826c43217b20ee53
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-29 13:57:57 +0200

    feat(abstract): adding iprucentreduction interface

--- Abstract-Factory/IVipShoppingFactory.cs
+++ Abstract-Factory/IVipShoppingFactory.cs
@@ -2,6 +2,11 @@ namespace design_patterns.Abstract_Factory;
 
 public interface IPriorityCommand
 {
+    int Priority { get;}
+}
+
+public interface IPurcentReduction
+{
     
 }
 public interface IVipShoppingFactory
