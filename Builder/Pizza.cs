d22ff653f32f88568aa05294952a80b768c86b7b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-13 15:42:46 +0200

    feat(builder): adding pizzabuilder class

--- Builder/Pizza.cs
+++ Builder/Pizza.cs
@@ -20,4 +20,11 @@ public class Pizza
     {
         return $"Pizza {Name}, ingredients : {string.Join(",", Ingredients)}";
     }
+    
+    public abstract class  PizzaBuilder
+    {
+        
+    }
+
+    der
 }
