7f517d4b8a92bced9dcee5d7246fcae1a1b7b485
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-13 15:31:20 +0200

    feat(builder): adding name & ingrnedients property

--- Builder/Pizza.cs
+++ Builder/Pizza.cs
@@ -2,5 +2,11 @@ namespace design_patterns.Builder;
 
 public class Pizza
 {
-    
+    public string Name { get; set; }
+    public List<string> Ingredients { get; set; }
+
+    public Pizza(string name)
+    {
+        
+    }
 }
